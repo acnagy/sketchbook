7dd0300d6e80ad35bf7cf1728af4c378a6262a60
Anna Nagy <anna@travis-ci.org>
2017-01-31 14:10:09 -0500

    test after-script status post

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo counting script
-for i in `seq 1 100000`;
-do	
-	if (( $i % 10000 == 0 )) ; 
-	then
-		echo $i
-	fi
-done
+echo $TEST_PROCESS
+export body='{
+	"state":"success",
+	"description":"travis-ci-jobs",
+	"target_url":"https://travis-ci.org/acnagy/sketchbook/jobs/'${TRAVIS_JOB_ID}'",
+	"context":"'${TEST_PROCESS}'"
+}'
 
-echo sleeping 1 minute
-sleep 1m
-
-echo done
+curl -X POST "https://api.github.com/repos/acnagy/sketchbook/statuses/${TRAVIS_COMMIT}" \
+-d "$body" \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: token ${GH_TOKEN}"
+echo "Job status posted in commit"
